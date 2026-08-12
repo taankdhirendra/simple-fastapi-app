a85e355668704697bda7077e169ea39e867cca47
dhirendra <133599653+taankdhirendra@users.noreply.github.com>
2026-08-12 22:42:12 +0530

    Update welcome message in home endpoint

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to FastAPI running on AWS! Create By Dhirendra",
+        "message": "Welcome to FastAPI running on AWS! Create By Tarun",
         "hostname": socket.gethostname(),
         "environment": os.getenv("ENV", "development")
     }
